48beb03093b4bf6d50cd5f1d5658954abcee4cf5
Talip <talip@ctsafebook.com>
2026-01-06 12:21:50 -0500

    Show build commit in UI

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   BUSINESS_NAME,
   TAGLINE,
--- src/pages/ReviewsPage.tsx
+++ src/pages/ReviewsPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { Link } from "react-router-dom";
 
 const GOOGLE_RATING = 4.4;
